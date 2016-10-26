d1c55137b24da60f56e1694712aac7fa8c99b355
galex <ogajvoronaky@gmail.com>
2016-10-27 00:23:49 +0300

    Added cart resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products, only: [:index, :new, :create, :show, :destoy]
   resources :users
-  resources :carts
+  resources :carts, only: [:show]
+  resource  :cart, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
