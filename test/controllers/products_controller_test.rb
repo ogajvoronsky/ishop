19f98b13407978e1a84f5e6e09d234205444547e
galex <ogajvoronaky@gmail.com>
2016-11-08 21:48:46 +0200

    Commit#8

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_index_url
-    assert_response :success
-  end
 
 end
