f515394ffbd0f9e1e7257609745e82a9b4301492
galex <ogajvoronaky@gmail.com>
2016-11-11 15:36:46 +0200

    Refactor cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,11 @@
 class CartsController < ApplicationController
+  before_action :current_cart
 
   def show
-    @cart = Cart.find(session[:cart_id])
     @products = @cart.products
   end
 
   def add
-    @cart = current_cart
     @product = Product.find(params[:product_id])
     @cart.products << @product
 
@@ -14,13 +13,16 @@ class CartsController < ApplicationController
   end
 
   def destroy
-    @cart = current_cart
     @cart.destroy
     redirect_to cart_path
   end
 
+  def order
+
+  end
+
     private
     def current_cart
-      Cart.find(session[:cart_id])
+      @cart = Cart.find(session[:cart_id])
     end
 end
