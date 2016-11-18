beb7bdbc04dd087aa2ee53a0ec3fa2410b8b3df4
galex <ogajvoronaky@gmail.com>
2016-11-18 10:12:42 +0200

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+  attr_accessor :email, :password, :password_confirmation, :remember_me, :encrypted_password
 
   has_one :account, dependent: :destroy
   has_one :cart, :through => :account
