454b588f7a7a08f189e13823fb4b122b5fc40366
galex <ogajvoronaky@gmail.com>
2016-11-18 09:28:09 +0200

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   # Gmail setting
 
-  onfig.action_mailer.default_url_options = { :host => 'ishop-cursor.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'ishop-cursor.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
